949de4826fa96494cc8024de068b717e3337c91c
unknown <mouni.shalaby@gmail.com>
2023-01-13 21:55:12 +0200

    completed

--- Readme.md
+++ Readme.md
@@ -10,6 +10,7 @@ This is a full product management system that allows users to create, read, upda
 4. You can also edit or delete existing products/All products by clicking the corresponding buttons on the products table
 
 # Features
+* Validation:This code contains validation for all inputs, ensuring that no fields are left empty and that the product  count is less than 100. Attempting to create a new product with a count greater than 100 will result in an error. 
 * Create new products
 * View all products
 * Edit existing products
--- script.js
+++ script.js
@@ -29,35 +29,43 @@ if (localStorage.product != null) {
 }
 submit.onclick = () => {
   let newPro = {
-    title: title.value,
+    title: title.value.toLowerCase(),
     price: price.value,
     taxes: taxes.value,
     ads: ads.value,
     discount: discount.value,
     total: total.innerHTML,
     count: count.value,
-    category: category.value,
+    category: category.value.toLowerCase(),
   };
 
-  if (mood === "create") {
-    if (newPro.count > 1) {
-      for (let i = 0; i < newPro.count; i++) {
-        dataPro.push(newPro);
+  if (
+    title.value != "" &&
+    price.value != "" &&
+    taxes.value != "" &&
+    category.value != "" &&
+    newPro.count<100
+  ) {
+    if (mood === "create") {
+      if (newPro.count > 1) {
+        for (let i = 0; i < newPro.count; i++) {
+          dataPro.push(newPro);
+        }
       }
+    } else {
+      dataPro[tmp] = newPro;
+      mood = "create";
+      submit.innerHTML = "Create";
+      count.style.display = "block";
     }
-  } else {
-    dataPro[tmp] = newPro;
-    mood = "create";
-    submit.innerHTML = "Create";
-    count.style.display = "block";
+      clearData();
+
   }
+
   //save product in localStorage
   localStorage.setItem("product", JSON.stringify(dataPro));
   // call function to clear Inputs
 
-  clearData();
-
-  console.log(dataPro);
   showData();
 };
 
@@ -74,12 +82,12 @@ const clearData = () => {
 };
 //read
 const showData = () => {
-  getTotal()
+  getTotal();
   let table = "";
   for (let i = 0; i < dataPro.length; i++) {
     table += `
     <tr>
-    <td>${i}</td>
+    <td>${i+1}</td>
     <td>${dataPro[i].title}</td>
     <td>${dataPro[i].price}</td>
     <td>${dataPro[i].taxes}</td>
@@ -126,10 +134,65 @@ const updateData = (i) => {
   submit.innerHTML = "Update";
   mood = "update";
   tmp = i;
-  scroll ({
+  scroll({
     top: 0,
     behvaior: "smooth",
   });
 };
 //search
+let searchMood = "title";
+let getSearchMood = (id) => {
+  let search = document.getElementById("search");
+  if (id == "searchTitle") {
+    searchMood = "title";
+  } else {
+    searchMood = "category";
+  }
+  search.focus();
+  search.placeholder = "Search By " + searchMood;
+
+  search.value = "";
+  showData();
+};
+
+let searchData = (value) => {
+  let table = "";
+  for (let i = 0; i < dataPro.length; i++) {
+    if (searchMood == "title") {
+      if (dataPro[i].title.includes(value.toLowerCase())) {
+        table += `
+    <tr>
+    <td>${i}</td>
+    <td>${dataPro[i].title}</td>
+    <td>${dataPro[i].price}</td>
+    <td>${dataPro[i].taxes}</td>
+    <td>${dataPro[i].ads}</td>
+    <td>${dataPro[i].discount}</td>
+    <td>${dataPro[i].total}</td>
+    <td>${dataPro[i].category}</td>
+    <td><button onclick="updateData(${i})" id="update">Update </button></td>
+    <td><button onclick="deleteData(${i})" id="delete">Delete</button></td>
+  </tr>`;
+      }
+    } else {
+      if (dataPro[i].category.includes(value.toLowerCase())) {
+        table += `
+    <tr>
+    <td>${i}</td>
+    <td>${dataPro[i].title}</td>
+    <td>${dataPro[i].price}</td>
+    <td>${dataPro[i].taxes}</td>
+    <td>${dataPro[i].ads}</td>
+    <td>${dataPro[i].discount}</td>
+    <td>${dataPro[i].total}</td>
+    <td>${dataPro[i].category}</td>
+    <td><button onclick="updateData(${i})" id="update">Update </button></td>
+    <td><button onclick="deleteData(${i})" id="delete">Delete</button></td>
+  </tr>`;
+      }
+    }
+  }
+
+  document.getElementById("tbody").innerHTML = table;
+};
 //clean data
